211717852082832e9b1ba7af1a9e7600ceb7fd5b
rarma <rarman321123@gmail.com>
2024-04-10 11:49:18 +0300

    Initial commit

---
+++ src/test/java/com/rungroup/webTest/WebTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rungroup.webTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
