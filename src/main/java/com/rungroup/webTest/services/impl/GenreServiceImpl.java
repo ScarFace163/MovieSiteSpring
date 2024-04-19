a0baab5f21b61b31ad4265e63224631c97f61736
rarma <rarman1@yandex.ru>
2024-04-19 17:44:40 +0300

    Added one-to-many. From main acc

--- src/main/java/com/rungroup/webTest/services/impl/GenreServiceImpl.java
+++ src/main/java/com/rungroup/webTest/services/impl/GenreServiceImpl.java
@@ -1,6 +1,7 @@
 package com.rungroup.webTest.services.impl;
 
 import com.rungroup.webTest.dtos.GenreDto;
+import com.rungroup.webTest.models.Genre;
 import com.rungroup.webTest.models.Movie;
 import com.rungroup.webTest.repositories.GenreRepository;
 import com.rungroup.webTest.repositories.MovieRepository;
@@ -17,6 +18,16 @@ public class GenreServiceImpl implements GenreService {
 
     @Override
     public void createGenre(Long movieId, GenreDto genreDto) {
-        Movie movie = movieRepository.findById(movieId);
+        Movie movie = movieRepository.findById(movieId).get();
+        Genre genre = mapToGenre(genreDto);
+        genre.setMovie(movie);
+        genreRepository.save(genre);
+    }
+    private Genre mapToGenre(GenreDto genreDto) {
+        return Genre.builder()
+                .id(genreDto.getId())
+                .type(genreDto.getType())
+                .description(genreDto.getDescription())
+                .build();
     }
 }
